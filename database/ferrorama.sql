08637583f4998b50a39a1c1ea40fbbab35a33034
Leonardo-de-Aguiar <leonardo_n_aguiar@estudante.sesisenai.org.br>
2026-09-09 08:19:26 -0300

    add - terminei o banco de dados e fiz a conexão

--- database/ferrorama.sql
+++ database/ferrorama.sql
@@ -8,3 +8,43 @@ CREATE TABLE usuarios (
     numero_telefone VARCHAR(15) NOT NULL,
     senha VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE funcionarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    numero_telefone VARCHAR(15) NOT NULL,
+    senha VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE gerentes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    numero_telefone VARCHAR(15) NOT NULL,
+    senha VARCHAR(50) NOT NULL
+);
+
+
+CREATE TABLE estacoes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+);
+
+CREATE TABLE trens (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    modelo VARCHAR(100) NOT NULL,
+    estacao_id INT,
+    sensor_id INT,
+    FOREIGN KEY (estacao_id) REFERENCES estacoes(id),
+    FOREIGN KEY (sensor_id) REFERENCES sensores(id)
+);
+
+
+CREATE TABLE sensores (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    tipo VARCHAR(50) NOT NULL,
+    trem_id INT,
+    FOREIGN KEY (trem_id) REFERENCES trens(id)
+);
